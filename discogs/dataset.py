Pre-training task: - Add CometML logger - Add Normalization - Fix rolling augmentation

discogs/dataset.py
@@ -25,7 +25,7 @@ dataset = Dataset('discogs')
 @dataset.config
 def default_config():
     name = 'discogs'  # dataset name
-    normalize = False  # normalize dataset
+    normalize = True  # normalize dataset
     subsample = False  # subsample squares from the dataset
     roll = True  # apply roll augmentation
     fold = 1
@@ -54,11 +54,10 @@ class DiscogsDataset(TorchDataset):
         self.classes_num = classes_num
         self.augment = augment
         if augment:
-            print(f"Will agument data from {groundtruth_file}")
+            print(f"Will augment data from {groundtruth_file}")
 
         self.melspectrogram_size = clip_length * sample_rate // hop_size
         self.n_bands = n_bands
-        self.intro_outro_offset = self.melspectrogram_size
 
     def __len__(self):
         return self.length
@@ -92,8 +91,8 @@ class DiscogsDataset(TorchDataset):
     def load_melspectrogram(self, melspectrogram_file: pathlib.Path):
         frames_num = melspectrogram_file.stat().st_size // (2 * self.n_bands)  # each float16 has 2 bytes
         frames_to_read = min(self.melspectrogram_size, frames_num)
-        max_frame = frames_num - self.melspectrogram_size - self.intro_outro_offset
-        min_frame = self.intro_outro_offset
+        max_frame = frames_num - self.melspectrogram_size
+        min_frame = 0
 
         if max_frame < min_frame:
             max_frame = frames_num - self.melspectrogram_size
@@ -163,7 +162,7 @@ def get_ft_cls_balanced_sample_weights(train_groundtruth, num_of_classes,
 
 @dataset.command
 def get_ft_weighted_sampler(samples_weights=CMD(".get_ft_cls_balanced_sample_weights"),
-                            epoch_len=600000, sampler_replace=False):
+                            epoch_len=200000, sampler_replace=False):
     num_nodes = int(os.environ.get('num_nodes', 1))
     ddp = int(os.environ.get('DDP', 1))
     num_nodes = max(ddp, num_nodes)
@@ -194,7 +193,7 @@ def get_base_test_set(eval_groundtruth, base_dir, eval_base_dir):
 
 
 @dataset.command(prefix='roll_conf')
-def get_roll_func(axis=1, shift=None, shift_range=50):
+def get_roll_func(axis=-1, shift=None, shift_range=50):
     print("rolling...")
 
     def roll_func(b):
@@ -210,14 +209,27 @@ def get_roll_func(axis=1, shift=None, shift_range=50):
     return roll_func
 
 
+@dataset.command(prefix='norm_conf')
+def get_norm_func(norm_mean=2.06755686098554, norm_std=1.268292820667291):
+    print("normalizing...")
+
+    def norm_func(b):
+        x, i, y = b
+        x = torch.as_tensor(x)
+
+        x = (x - norm_mean) / (norm_std * 2)
+
+        return x, i, y
+
+    return norm_func
+
+
 @dataset.command
 def get_train_set(normalize, roll, wavmix=False):
     ds = get_base_train_set()
     # get_ir_sample()
     if normalize:
-        print("normalized train!")
-        fill_norms()
-        ds = PreprocessDataset(ds, norm_func)
+        ds = PreprocessDataset(ds, get_norm_func())
     if roll:
         ds = PreprocessDataset(ds, get_roll_func())
 
@@ -229,9 +241,7 @@ def get_train_set(normalize, roll, wavmix=False):
 def get_test_set(normalize):
     ds = get_base_test_set()
     if normalize:
-        print("normalized test!")
-        fill_norms()
-        ds = PreprocessDataset(ds, norm_func)
+        ds = PreprocessDataset(ds, get_norm_func())
     return ds
 
 

ex_discogs.py
@@ -1,8 +1,15 @@
+import comet_ml
+
+import matplotlib as mpl
+mpl.use('Agg')
+
 import os
+from pathlib import Path
 import sys
 
 import torch
 from pytorch_lightning.callbacks import ModelCheckpoint
+from pytorch_lightning.loggers import CometLogger
 from sacred.config_helpers import DynamicIngredient, CMD
 from torch.nn import functional as F
 import numpy as np
@@ -11,6 +18,8 @@ from ba3l.experiment import Experiment
 from ba3l.module import Ba3lModule
 
 from torch.utils.data import DataLoader
+from tqdm import tqdm
+import matplotlib.pyplot as plt
 
 from config_updates import add_configs
 from helpers.mixup import my_mixup
@@ -71,7 +80,7 @@ def default_conf():
             fmax=None,
             norm=1,
             fmin_aug_range=10,
-            fmax_aug_range=2000
+            fmax_aug_range=2000,
         )
     }
     basedataset = DynamicIngredient("discogs.dataset.dataset", wavmix=1)
@@ -162,6 +171,13 @@ class M(Ba3lModule):
             rn_indices, lam = my_mixup(batch_size, self.mixup_alpha)
             lam = lam.to(x.device)
             x = x * lam.reshape(batch_size, 1, 1, 1) + x[rn_indices] * (1. - lam.reshape(batch_size, 1, 1, 1))
+
+        for i in range(len(x)):
+            if not Path(f"example{i}.png").exists():
+                patch = x[i].detach().cpu().numpy().squeeze()
+                plt.matshow(patch, aspect="auto")
+                plt.colorbar()
+                plt.savefig(f"example{i}.png")
 
         y_hat, embed = self.forward(x)
 
@@ -305,6 +321,14 @@ def main(_run, _config, _log, _rnd, _seed):
 
     modul = M(ex)
 
+    if os.environ["NODE_RANK"] == "0":
+        comet_logger = CometLogger(
+            project_name=_config["basedataset"]["name"],
+            api_key=os.environ["COMET_API_KEY"],
+            )
+        trainer.logger = comet_logger
+        comet_logger.log_hyperparams(_config)
+
     trainer.fit(
         modul,
         train_dataloader=train_loader,
@@ -389,6 +413,21 @@ def evaluate_only(_run, _config, _log, _rnd, _seed):
     print("\n\n Validtaion:")
     print(res)
 
+@ex.command
+def compute_norm_stats(_run, _config, _log, _rnd, _seed):
+    # force overriding the config, not logged = not recommended
+    loader = ex.get_train_dataloaders()
+    mean = []
+    std = []
+
+    for i, (audio_input, _, _) in tqdm(enumerate(loader), total=len(loader)):
+        audio_input = audio_input.type(torch.DoubleTensor)
+        cur_mean = torch.mean(audio_input)
+        cur_std = torch.std(audio_input)
+        mean.append(cur_mean)
+        std.append(cur_std)
+        # print(cur_mean, cur_std, np.max(audio_input), np.min(audio_input))
+    print(np.mean(mean), np.mean(std))
 
 @ex.command
 def test_loaders():