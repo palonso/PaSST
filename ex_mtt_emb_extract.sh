0bacb58f15db27f3e1ec98c9ac8b1d148317453e
palonso <pablo.alonso@upf.edu>
2023-03-28 12:34:10 +0200

    Add routine to extract embeddings

--- ex_mtt_emb_extract.sh
+++ ex_mtt_emb_extract.sh
@@ -1,5 +1,6 @@
-for n_block in 6
+for secs in 10 20 30
 do
+
     CUDA_VISIBLE_DEVICES=0 python ex_discogs.py \
         extract_embeddings with \
         trainer.precision=16 \
@@ -7,9 +8,8 @@ do
         basedataset.base_dir="/home/palonso/data/magnatagatune-melspectrograms/" \
         norm_conf.norm_mean=1.5880631462493773 \
         norm_conf.norm_std=1.1815654825219488 \
-        passt_discogs_30sec \
-        inference.n_block=$n_block \
-        passt_discogs30sec_inference
-done
+        inference.n_block=6 \
+        passt_discogs_"$secs"sec \
+        passt_discogs"$secs"sec_inference
 
-    # models.net.checkpoint="/home/palonso/reps/PaSST/output/discogs/230323-020758/discogs/d42e938b9d2d4d389c555c2f508fe277/checkpoints/epoch=129-step=541709.ckpt" \
+done
